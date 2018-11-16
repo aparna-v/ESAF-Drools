dc11acb3b57ac7cb48a1a9241381965d173758e6
admin <>
2018-11-16 00:44:46 +0000

    {/src/main/java/com/artoo/esaf/CreditScoreSection.java}

--- src/main/java/com/artoo/esaf/CreditScoreSection.java
+++ src/main/java/com/artoo/esaf/CreditScoreSection.java
@@ -13,6 +13,9 @@ public class CreditScoreSection implements java.io.Serializable {
 	@org.kie.api.definition.type.Label("Section Name")
 	private java.lang.String section_name;
 
+	@org.kie.api.definition.type.Label(value = "Parameters")
+	private java.util.List<com.artoo.esaf.CreditScoringParameter> parameters;
+
 	public CreditScoreSection() {
 	}
 
@@ -32,10 +35,21 @@ public class CreditScoreSection implements java.io.Serializable {
 		this.section_name = section_name;
 	}
 
+	public java.util.List<com.artoo.esaf.CreditScoringParameter> getParameters() {
+		return this.parameters;
+	}
+
+	public void setParameters(
+			java.util.List<com.artoo.esaf.CreditScoringParameter> parameters) {
+		this.parameters = parameters;
+	}
+
 	public CreditScoreSection(java.lang.Double section_score,
-			java.lang.String section_name) {
+			java.lang.String section_name,
+			java.util.List<com.artoo.esaf.CreditScoringParameter> parameters) {
 		this.section_score = section_score;
 		this.section_name = section_name;
+		this.parameters = parameters;
 	}
 
 }
